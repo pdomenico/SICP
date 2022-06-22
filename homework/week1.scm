94086c7a05f71d6aa0e155aff8c0d63440f9fb3b
pdomenico <domenicoprocopio.a@gmail.com>
2022-06-22 19:43:22 +0200

    added last w1 exercises

--- homework/week1.scm
+++ homework/week1.scm
@@ -93,3 +93,30 @@
     (cond ((empty? phrase) '())
           ((wordEnds-e (first phrase)) (sentence (first phrase) (ends-e (bf phrase))))
           (else (ends-e (bf phrase)))))
+
+
+; 6. Most versions of Lisp provide and and or procedures like the ones on page 19. In
+; principle there is no reason why these can’t be ordinary procedures, but some versions of
+; Lisp make them special forms. Suppose, for example, we evaluate
+; (or (= x 0) (= y 0) (= z 0))
+; If or is an ordinary procedure, all three argument expressions will be evaluated before or
+; is invoked. But if the variable x has the value 0, we know that the entire expression has
+; to be true regardless of the values of y and z. A Lisp interpreter in which or is a special
+; form can evaluate the arguments one by one until either a true one is found or it runs out
+; of arguments.
+; Your mission is to devise a test that will tell you whether Scheme’s and and or are special
+; forms or ordinary functions. This is a somewhat tricky problem, but it’ll get you thinking
+; about the evaluation process more deeply than you otherwise might.
+; Why might it be advantageous for an interpreter to treat or as a special form and evaluate
+; its arguments one at a time? Can you think of reasons why it might be advantageous to
+; treat or as an ordinary function?
+
+(define (test x)
+
+    (if (or (= x 0) (= y 0))
+        '(condition true)
+        '(condition false)))
+
+; If 0 as argument is provided, the function does not cause an error (although y is not defined),
+; which means that scheme does not check for (= y 0). Otherwise, if x != 0, the function will 
+; generate an error "test: variable 'y' unbound"
