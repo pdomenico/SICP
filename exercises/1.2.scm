49c979dffcd0863a04b041bcb0683d00ce6a4650
pdomenico <domenicoprocopio.a@gmail.com>
2022-06-30 20:26:34 +0200

    added exercises 1.2

--- exercises/1.2.scm
+++ exercises/1.2.scm
@@ -22,3 +22,32 @@
                           last
                           (+ 1 count)))))
     (iter 0 0 0 1))
+
+; Exercise 1.16
+; Design a procedure that evolves an iterative exponentiation process that uses successive squaring
+; and uses a logarithmic number of steps, as does fast-expt.
+; (Hint: Using the observation that (bn/2)2 = (b2)n/2, keep, along with the exponent n and the base b,
+; an additional state variable a, and define the state transformation in such a way that
+; the product a bn is unchanged from state to state.
+; At the beginning of the process a is taken to be 1, and the answer is given 
+; by the value of a at the end of the process. 
+; In general, the technique of defining an invariant quantity that remains unchanged from state to state
+; is a powerful way to think about the design of iterative algorithms.)
+
+(define (even? n) (= 0 (remainder n 2)))
+(define (square n) (* n n))
+
+; Recurrent version of fast-exp
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+; Iterative version of fast-exp
+(define (iter-fast-expt b n)
+    (define (iter a b exponent)
+        (cond ((= exponent 0) 1)
+              ((= exponent 1) (* a b))
+              ((even? exponent) (iter a (square b) (/ exponent 2)))
+              (else (iter (* a b) b (- exponent 1)))))
+    (iter 1 b n))
